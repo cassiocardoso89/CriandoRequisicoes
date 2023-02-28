252a626abe4841a4c47f24b4b4297e862893373c
Cassio de Souza Cardoso <cassio.cardoso@sequor.com.br>
2023-02-27 23:34:18 -0300

    fix nodeserver, preenchendo lista de videos com json, iniciando formulario criarVideo

--- requisicoes-main/js/conectaAPI.js
+++ requisicoes-main/js/conectaAPI.js
@@ -1,6 +1,28 @@
 async function ListaVideos(){
-    const connection = await fetch("http://localhost:3000/videos");
-    console.log(connection);
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
 }
 
-ListaVideos();
+async function CriaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos",{
+        method:"POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem:imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
+export const conectaAPI = {
+    ListaVideos,
+    CriaVideo
+}
