c0a14721e9fe7b69e61f3e1c8b3c9d5cf53b3693
Cassio de Souza Cardoso <cassio.cardoso@sequor.com.br>
2023-02-27 23:49:02 -0300

    cadastra video concluido

--- requisicoes-main/js/criarVideo.js
+++ requisicoes-main/js/criarVideo.js
@@ -1,13 +1,17 @@
+import { conectaAPI } from "./conectaAPI.js";
 const formulario = document.querySelector("[data-formulario]");
 
-function CriarVideo(evento){
+async function CriarVideo(evento){
     evento.preventDefault();
-    
+
     const imagem = document.querySelector("[data-imagem]").value;
     const url = document.querySelector("[data-url]").value;
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString();
 
+    await conectaAPI.CriaVideo(titulo, descricao, url, imagem);
+
+    window.location.href = "../pages/envio-concluido.html";
 }
 
 formulario.addEventListener("submit", evento => CriarVideo(evento));
